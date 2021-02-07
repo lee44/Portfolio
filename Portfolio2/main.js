3cc937fc0aad982a08fb949368b825af68dd9b49
Lee44 <jlee7772@yahoo.com>
2021-02-06 19:05:34 -0800

    Forgot the top of the page button. Tutorial Finished.

--- Portfolio2/main.js
+++ Portfolio2/main.js
@@ -16,6 +16,12 @@ $(document).ready(function () {
         },2000);
     })
 
+    $('#up').on('click',function(){
+        $('html, body').animate({
+            scrollTop: 0
+        },500);
+    })
+
     AOS.init({
         easing:'ease',
         duration: 1800,
