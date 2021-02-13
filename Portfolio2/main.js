11ab6f2e96f946bff78816a7a175c03a3bb78fd5
Lee44 <jlee7772@yahoo.com>
2021-02-12 23:45:28 -0800

    Navbar section is selected by scroll bar position

--- Portfolio2/main.js
+++ Portfolio2/main.js
@@ -26,12 +26,33 @@ $(document).ready(function () {
     })
 
     up = document.getElementById("up");
+    skills_coord = $('#skills').offset().top - 200;
+    port_coord = $('#portfolio').offset().top - 200;
     var myScrollFunc = function() {
         var y = window.scrollY;
+
+        // Toggles the Scroll Top Button
         if (y >= 400) {
             up.style.display = "flex"
         } else {
             up.style.display = "none"
+        }
+
+        // Highlights the section in navbar based on scroll position
+        if(y < skills_coord)
+        {
+            $('nav a[href]').removeClass('active');
+            $('nav a[href*="#home"]').toggleClass('active');
+        }
+        else if(y >= skills_coord && y < port_coord)
+        {
+            $('nav a[href]').removeClass('active');
+            $('nav a[href*="#skills"]').toggleClass('active');
+        }
+        else if(y >= port_coord)
+        {
+            $('nav a[href]').removeClass('active');
+            $('nav a[href*="#portfolio"]').toggleClass('active');
         }
     };
       
