1a883d812a704eeb0f81664bc5b8af112b5766b5
lee44 <jlee7772@yahoo.com>
2021-02-13 20:53:22 -0800

    Finished Logo

--- Portfolio2/main.js
+++ Portfolio2/main.js
@@ -19,6 +19,12 @@ $(document).ready(function () {
         $(this).toggleClass('active');    
     })
 
+    $('.logo').on('click',function(){
+        $('html, body').animate({
+            scrollTop: $('#home').offset().top
+        },500);  
+    })
+
     $('#up').on('click',function(){
         $('html, body').animate({
             scrollTop: 0
